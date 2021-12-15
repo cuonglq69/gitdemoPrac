8c83fd2944d09ec959564cbc9c21fda5d1e06849
cuonglq69 <cuonglq69@gmail.com>
2021-12-15 23:13:03 +0700

    add all file

---
+++ src/gitDemo/TestCode.java
@@ -0,0 +1,11 @@
+package gitDemo;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the demo for git");
+		System.out.println("More code is added");
+	}
+
+}
